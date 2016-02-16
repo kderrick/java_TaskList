a3231d91f01bea636e39c97e2c7ee8dc9942a4b3
Kyle Derrick and Ryan King <kderrick1988@gmail.com>
2016-02-16 09:04:55 -0800

    finish Task app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,38 @@
 import java.util.HashMap;
-
+import java.util.ArrayList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+      staticFileLocation("/public");
+      String layout = "templates/layout.vtl";
+      get("/", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("template", "templates/index.vtl");
+        model.put("tasks", request.session().attribute("tasks"));
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
 
+      post("/tasks", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
 
-        staticFileLocation("/public");
-        String layout = "templates/layout.vtl";
+      ArrayList<Task> tasks = request.session().attribute("tasks");
+
+        if (tasks == null) {
+          tasks = new ArrayList<Task>();
+          request.session().attribute("tasks", tasks);
+        }
+
+      String description = request.queryParams("description");
+      Task newTask = new Task(description);
+
+      tasks.add(newTask);
+
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
         //RESTful ARCHITECTURE
         //Use POST to create something on the server
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -20,6 +20,40 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
+  @Test
+  public void rootTest() {
+      goTo("http://localhost:4567/");
+      assertThat(pageSource()).contains("Task list!");
+    }
 
-  //Tests go here
+  @Test
+  public void taskIsCreatedTest() {
+      goTo("http://localhost:4567/");
+      fill("#description").with("Mow the lawn");
+      submit(".btn");
+      assertThat(pageSource()).contains("Your task has been saved.");
+      }
+
+  @Test
+  public void taskIsDisplayedTest() {
+      goTo("http://localhost:4567/");
+      fill("#description").with("Mow the lawn");
+      submit(".btn");
+      click("a", withText("Go Back"));
+      assertThat(pageSource()).contains("Mow the lawn");
+  }
+
+  @Test
+  public void multipleTasksAreDisplayedTest() {
+    goTo("http://localhost:4567/");
+    fill("#description").with("Mow the lawn");
+    submit(".btn");
+    click("a", withText("Go Back"));
+    fill("#description").with("Buy groceries");
+    submit(".btn");
+    click("a", withText("Go Back"));
+    assertThat(pageSource()).contains("Mow the lawn");
+    assertThat(pageSource()).contains("Buy groceries");
+  }
+
 }
